bc63aa1454c3254ddcf4c120f299338105d16c20
musasazib <musasazib@gmail.com>
2021-09-29 17:25:50 +0600

    Router, route, path, component, not found, exact

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
+import About from './components/About/About';
 import Friends from './components/Home/Friends/Friends';
 
 function App() {
   return (
     <div className="App">
-      <Friends></Friends>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
