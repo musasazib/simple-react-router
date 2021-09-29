acf09aecd1031476a81f2da2da0c52d7e267f0c2
musasazib <musasazib@gmail.com>
2021-09-29 18:17:36 +0600

    Create route with some fixed and some dynamic part

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NotFound from './components/NotFound/NotFound';
 import About from './components/About/About';
 import Friends from './components/Home/Friends/Friends';
 import Header from './components/Header/Header';
+import FriendDetail from './components/FriendDetail/FriendDetail';
 
 function App() {
   return (
@@ -20,6 +21,9 @@ function App() {
           </Route>
           <Route path="/friends">
             <Friends></Friends>
+          </Route>
+          <Route path="/friend/:fdid">
+            <FriendDetail></FriendDetail>
           </Route>
           <Route path="/about">
             <About></About>
--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Friend.css';
 
 const Friend = (props) => {
-    const { name, email, address, phone, website } = props.friend;
+    const { id, name, email, address, phone, website } = props.friend;
+    const url =`/friend/${id}`
     return (
         <div className="friend-container">
             <h3>I am {name}</h3>
@@ -10,6 +12,7 @@ const Friend = (props) => {
             <h4>Email me {email}</h4>
             <h4>Visit me {website}</h4>
             <h5>I live in {address.city}</h5>
+            <Link to={url}>Visit Me</Link>
         </div>
     );
 };
